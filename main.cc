b9f48545a476b320cda5d2cb17ed25235d5e5236
Oren <>
2023-06-08 23:54:49 -0700

    Main

---
+++ main.cc
@@ -0,0 +1,18 @@
+#include <seastar/core/app-template.hh>
+#include <seastar/util/log.hh>
+#include <iostream>
+#include <stdexcept>
+
+extern seastar::future<> f();
+
+int main(int argc, char** argv) {
+    seastar::app_template app;
+    try {
+      app.run(argc, argv, f);
+    } catch(...) {
+      std::cerr << "Couldn't start application: "
+		<< std::current_exception() << "\n";
+      return 1;
+    }
+    return 0;
+}
